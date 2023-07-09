681169b85792871ec7c800ed43d1b1fd7425323c
marszman <136658905+marszman@users.noreply.github.com>
2023-07-09 22:40:17 +0000

    rps - initial commit, organized

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
-require 'sinatra'
+# /dice.rb
 
-get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  erb(:rules)
+end
+
+get("/paper") do
+  erb(:paper)
+end
+
+get("/rock") do
+  erb(:rock)
+end
+
+get("/scissors") do
+  erb(:scissors)
 end
